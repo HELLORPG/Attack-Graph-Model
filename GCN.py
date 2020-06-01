5133f53e61defa3a7f9127903659f527927d75c3
HELLORPG <hellorpg2017@gmail.com>
2020-06-01 22:05:50 +0800

    [add] 完成了将Data类组织的数据放入GPU

--- GCN.py
+++ GCN.py
@@ -8,7 +8,25 @@ import OpData
 from torch_geometric.data import Data   # 用于承载Graph Data
 
 
+def GraphData() -> Data:
+    """
+    :return: 得到一个torch_geometric框架下支持的Graph Data
+    """
+    X = torch.tensor(OpData.GetFeatures(), dtype=torch.float)
+    Y = torch.tensor(OpData.GetLabels(), dtype=torch.int)
+    edges = torch.tensor(OpData.GetAdj(), dtype=torch.long)
+    data = Data(x=X, y=Y, edge_index=edges)
+    data.to(torch.device('cuda'))
+    return data
+
+
 class Classifier(torch.nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()  # 调用父类初始化
 
+
+def main():
+    data = GraphData()
+
+
+main()
